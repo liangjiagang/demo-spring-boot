074eb8529b12262fe6dd82bc2511adf41869b3e4
liangjiagang <458468364@qq.com>
2023-08-10 14:32:28 +0800

    【demo-roketmq】

---
+++ demo-rocketmq/src/main/java/com/alagang/demo/springboot/demorocketmq/DemoRocketmqApplication.java
@@ -0,0 +1,13 @@
+package com.alagang.demo.springboot.demorocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRocketmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRocketmqApplication.class, args);
+	}
+
+}
---
+++ demo-rocketmq/src/test/java/com/alagang/demo/springboot/demorocketmq/DemoRocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alagang.demo.springboot.demorocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRocketmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
